2557cc701942d4b07a58ca735ce82553862d36a6
SenLief <senlief@163.com>
2017-12-01 01:14:30 +0800

    modify text

--- celery_app/task.py
+++ celery_app/task.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 
+from datetime import datetime, timedelta
 from celery_app.celery_ding import app
 from pymongo import MongoClient
 from celery_app.DingTalkBot import Message
@@ -29,7 +30,17 @@ def push(objectId, token):
         at_mobiles = ' '.join(['@' + x for x in phones])
     finally:
         pass
-    text = "> ## 信息推送\n\n### {0}\n\n".format(at_mobiles) + "#### 姓名：{0}\n\n#### 学校：{1}\n\n#### 联系人姓名：{2}\n\n#### 联系人方式：{3}\n\n#### 报名分校：{4}\n\n#### 学生年级：{5}\n\n#### 预报科目：{6}\n\n#### 报名时间：{7}".format(
-        post['field_1'], post['field_2'], post['field_4'], post['field_8'], post['field_6'], post['field_7'], "，".join(post['field_9']), post['created_at'].split(" ")[1])
+    utc_dt = datetime.strptime(post['created_at'][:-4], '%Y-%m-%d %H:%M:%S')
+    local_dt = utc_dt + timedelta(hours=8)
+    created_time = datetime.strftime(local_dt, '%Y-%m-%d %H:%M:%S')
+
+    text = "> ## 信息推送\n\n### {0}\n\n".format(at_mobiles) + "#### 序号：{0}\n\n#### 姓名：{1}\n\n#### 学校：{2}\n\n#### 联系人姓名：{3}\n\n#### 联系人方式：{4}\n\n#### 报名分校：{5}\n\n#### 学生年级：{6}\n\n#### 预报科目：{7}\n\n#### 报名时间：{8}".format(
+        post['serial_number'], post['field_1'], post['field_2'], post['field_4'], post['field_8'], post['field_6'], post['field_7'], "，".join(post['field_9']), created_time)
     msg = Message(webhook)
     return msg.send_markdown(title, text, phones)
+
+
+if __name__ == '__main__':
+    obid = ''
+    token = '3ad6ce86f476952c6c3f5ff010bfc471a2bcdbd6349db4fe20c2b9984249b9d2'
+    push(obid, token)
